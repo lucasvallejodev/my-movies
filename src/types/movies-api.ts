86053e0fa72a3300e881e782072d4bc0cead485e
Lucas Vallejo <97030519+lucasvallejodev@users.noreply.github.com>
2025-07-28 22:50:40 +0200

    feat: add movie details types

--- src/types/movies-api.ts
+++ src/types/movies-api.ts
@@ -13,11 +13,58 @@ export type Movie = {
   video: boolean;
   vote_average: number;
   vote_count: number;
-}
+};
 
 export type MoviesResponse = {
   page: number;
   results: Movie[];
   total_pages: number;
   total_results: number;
+};
+
+export type ProductionCompany = {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+export type ProductionCountry = {
+  iso_3166_1: string;
+  name: string;
+}
+
+export type SpokenLanguage = {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export type Collection = {
+  id: number;
+  name: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+}
+
+export type MovieDetail = {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  release_date: string;
+  vote_average: number;
+  vote_count: number;
+  runtime: number;
+  genres: { id: number; name: string }[];
+  homepage: string | null;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  spoken_languages: SpokenLanguage[];
+  belongs_to_collection: Collection | null;
+  budget: number;
+  revenue: number;
+  status: string;
+  tagline: string | null;
 }
