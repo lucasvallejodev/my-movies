0a0c818fb5fee9d1f634b8c8fd4c7b95fb2dd7c8
Lucas Vallejo <97030519+lucasvallejodev@users.noreply.github.com>
2025-07-29 10:30:29 +0200

    feat: update favorites page

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import './favorites.scss';
+import { useFavorites } from '../hooks/use-favorites';
+import Carousel from '../components/carousel/carousel';
 
 function Favorites() {
+  const { getAllFavorites, favoritesCount } = useFavorites();
+  const favoriteMovies = getAllFavorites();
+
+  if (favoritesCount === 0) {
+    return (
+      <div className="favorites-page">
+        <h1>Your Favorite Movies</h1>
+        <div className="empty-favorites">
+          <p>No favorite movies yet</p>
+          <p className="empty-message-secondary">
+            Browse movies and click the heart icon to add them to your favorites!
+          </p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="favorites-page">
-      <h1>Your Favorite Movies</h1>
-      <p>Movies you've marked as favorites will appear here</p>
+      <h1>Your Favorite Movies ({favoritesCount})</h1>
+      <Carousel items={favoriteMovies} title="Your Favorites" />
     </div>
-  )
+  );
 }
 
-export default Favorites
+export default Favorites;
